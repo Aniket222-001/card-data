3c26d10831ae627046e6f7e4ad48a3c3dc5fae40
Aniket222-001 <139674804+Aniket222-001@users.noreply.github.com>
2024-03-01 23:43:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,22 @@ App.use(cors())
 //   next();
 // });
 
-const mongodb = async()=>{
-await mongoose.connect('mongodb+srv://aniketkushwaha0408:Aniket12345@login.gvlzkmg.mongodb.net/?retryWrites=true&w=majority&appName=login/Login')
-.then(()=>{
-    console.log('connected to database')
-})
-.catch(()=>{
-    console.log('failed to connect to database')
-})
-}
+const mongodb = async () => {
+  try {
+    await mongoose
+      .connect(
+        "mongodb+srv://aniketkushwaha0408:Aniket12345@login.gvlzkmg.mongodb.net/?retryWrites=true&w=majority&appName=login/Login"
+      )
+      .then(() => {
+        console.log("connected to database");
+      })
+      .catch(() => {
+        console.log("failed to connect to database");
+      });
+  } catch (e) {
+    console.log(e);
+  }
+};
 mongodb()
 
 // mongoose.connect('mongodb+srv://aniketkushwaha0408:Aniket12345@login.gvlzkmg.mongodb.net/?retryWrites=true&w=majority&appName=login/Login')
